f49be1f5cb7b7debdc8b8536a2600c050a7db648
Shifat507 <ferdoushasanshifat@gmail.com>
2025-02-24 16:26:19 +0600

    added Navbar and Banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,41 @@
+"use client"; 
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+import slider1 from '../assets/banner1.jpg';
+import slider2 from '../assets/banner2.jpg';
+import slider3 from '../assets/banner3.jpg';
+import slider4 from '../assets/banner4.jpg';
+import slider5 from '../assets/banner5.jpg';
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel >
+                <div>
+                    <Image src={slider1} alt="Banner 1" />
+                    {/* <p className="legend">Legend 1</p> */}
+                </div>
+                <div>
+                    <Image src={slider2} alt="Banner 2" />
+                    {/* <p className="legend">Legend 2</p> */}
+                </div>
+                <div>
+                    <Image src={slider3} alt="Banner 3" />
+                    {/* <p className="legend">Legend 3</p> */}
+                </div>
+                <div>
+                    <Image src={slider4} alt="Banner 4" />
+                    {/* <p className="legend">Legend 4</p> */}
+                </div>
+                <div>
+                    <Image src={slider5} alt="Banner 5" />
+                    {/* <p className="legend">Legend 5</p> */}
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
